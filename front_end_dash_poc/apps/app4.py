4a16010fd63c909bdce08ed4c75070ae85fc23b2
gaylej <j.gayle@elsvier.com>
2021-01-21 21:16:21 +0000

    app 4 updated

--- front_end_dash_poc/apps/app4.py
+++ front_end_dash_poc/apps/app4.py
@@ -11,16 +11,23 @@ from app import app
 # mapbox styles that do not require a token: open-street-map, white-bg, carto-positron, carto-darkmatter, stamen-terrain, stamen-toner, stamen-watercolor
 # color scale informaiton: https://plotly.com/python/builtin-colorscales/
 
-df = pd.read_csv('admission_pollution_melt.csv').fillna(0)
-df = df[df['Indicator Name'] != 'heathrow_distance']
+df = pd.read_csv('health_pollution_final.csv').fillna(0)
+
+# set indicators and CCG locations for hhe drop downs
 available_indicators = df['Indicator Name'].unique()
-
-
 available_locations = list(df['Area Name'].unique())
 available_locations.append('All CCGs')
 
+
+# do a groupby to get the correct format for line plots
+df_f = df.groupby(['Year', 'Radius Location', 'Indicator Name']).sum('Value').reset_index()
+df_f = df_f[df_f['Indicator Name'] == 'COPD Admissions']
+
+fig122 = px.line(df_f[df_f['Value'] != 0], x='Year', y='Value', color='Radius Location')
+fig122.update_yaxes(title='COPD Admissions')
+
 layout = html.Div([
-    html.H3('(Joseph - DS4A Team 27 - Air Pollution and Health Ailments around Heathrow Airport'),
+    html.H3('(Inspired By Seb Joseph - DS4A Team 27 - Air Pollution and Health Ailments around Heathrow Airport'),
     html.Div([
 
         html.Div([
@@ -28,6 +35,7 @@ layout = html.Div([
                 id='indicator-column',
                 options=[{'label': i, 'value': i} for i in available_indicators],
                 value='Heart Failure Admissions',
+                clearable=False
             ),
         ],
             style={'width': '48%', 'display': 'inline-block'}),
@@ -37,6 +45,7 @@ layout = html.Div([
                 id='ccg-column',
                 options=[{'label': i, 'value': i} for i in available_locations],
                 value='All CCGs',
+                clearable=False
             ),
         ], style={'width': '49%', 'float': 'right', 'display': 'inline-block'}),
     ], style={
@@ -46,9 +55,18 @@ layout = html.Div([
     }),
 
     html.Div([
-        dcc.Graph(
-            id='crossfilter-indicator-scattermap')
+        html.Div([
+            dcc.Graph(
+                id='crossfilter-indicator-scattermap'
+            )
+        ], style={'width': '48%', 'display': 'inline-block'}),
+        html.Div([
+            dcc.Graph(
+                id='crossfilter-indicator-density-mapbox'
+            )
+        ], style={'width': '49%', 'display': 'inline-block', 'float': 'right'}),
     ]),
+
 
     html.Div(dcc.Slider(
         id='crossfilter-year-slider',
@@ -59,7 +77,59 @@ layout = html.Div([
         step=None
     ), style={'width': '49%', 'padding': '0px 20px 20px 20px'}),
 
-    html.Div(id='app-1-display-value'),
+    html.Div([
+
+        html.Div([
+            html.Div([dcc.Dropdown(
+                id='indicator-health-dropdown',
+                options=[{'label': i, 'value': i} for i in available_indicators],
+                value='Heart Failure Admissions',
+                clearable=False
+            )]),
+
+            html.Div([
+
+                html.Div([
+                    dcc.RadioItems(
+                        id='crossfilter-avg-sum',
+                        options=[{'label': i, 'value': i} for i in ['Average', 'Sum']],
+                        value='Average',
+                        labelStyle={'display': 'inline-block'}
+                    ),
+
+                ],style={'width': '49%', 'display': 'inline-block'}),
+
+                html.Div([
+                    dcc.RadioItems(
+                        id='radius-nonradius',
+                        options=[{'label': i, 'value': i} for i in ['Inner/Outer CCG Breakdown', 'All CCGs']],
+                        value='Inner/Outer CCG Breakdown',
+                        labelStyle={'display': 'inline-block'}
+                    ),
+
+                ],style={'width': '49%', 'display': 'inline-block', 'float': 'right'})
+
+
+            ],style={'padding': '20px 20px 20px 20px'}),
+
+            html.Div([dcc.Graph(figure=fig122, id='crossfilter-line-health')]),
+
+        ], style={'width': '48%', 'display': 'inline-block'}),
+
+        html.Div([
+            html.Div([dcc.Dropdown(
+                id='indicator-column435',
+                options=[{'label': i, 'value': i} for i in available_indicators],
+                value='Heart Failure Admissions',
+                clearable=False
+            )]),
+
+            html.Div([dcc.Graph(figure=fig122)]),
+
+        ], style={'width': '49%', 'display': 'inline-block', 'float': 'right'}),
+
+
+    ]),
 
     # Navigation Tree - Don't Delete
     html.Div(dcc.Link('Go to App 1', href='/apps/app1')),
@@ -96,10 +166,63 @@ def update_graph(indicator_name, ccg_name,
     return fig
 
 
-#### Density heatmap example ### -- disregarded in favour of the scatter mapbox
+@app.callback(
+    dash.dependencies.Output('crossfilter-indicator-density-mapbox', 'figure'),
+    [dash.dependencies.Input('indicator-column', 'value'),
+    dash.dependencies.Input('ccg-column', 'value'),
+     dash.dependencies.Input('crossfilter-year-slider', 'value')
+     ])
+def update_density_graph(indicator_name, ccg_name,
+                 year_value):
 
-# quakes = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/earthquakes-23k.csv')
-# fig = go.Figure(go.Densitymapbox(lat=df.LAT, lon=df.LONG, z=df.Value,
-#                                  radius=10))
-# fig.update_layout(mapbox_style="stamen-terrain", mapbox_center_lon=180)
-# fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+    if ccg_name == 'All CCGs':
+        dff = df
+    else:
+        dff = df[df['Area Name'] == ccg_name]
+
+    dff = dff[dff['Value'] != 0]
+
+    dff = dff[dff['Indicator Name'] == indicator_name]
+    dff = dff[dff['Year'] == year_value]
+
+    fig_f = px.density_mapbox(dff, lat='LAT', lon='LONG', z='Value', radius=40, zoom=7, center=dict(lat=51.47, lon=0),
+                              mapbox_style="open-street-map", hover_name='Area Name',
+                              hover_data=['Indicator Name', 'Value', 'LAT', 'LONG'])
+    return fig_f
+
+
+
+@app.callback(
+    dash.dependencies.Output('crossfilter-line-health', 'figure'),
+    [dash.dependencies.Input('indicator-health-dropdown', 'value'),
+    dash.dependencies.Input('crossfilter-avg-sum', 'value'),
+    dash.dependencies.Input('radius-nonradius', 'value'),
+     ])
+def update_health_line(indicator_name, avg_sum, radius_toggle):
+    if avg_sum == 'Average':
+        if radius_toggle == 'Inner/Outer CCG Breakdown':
+            dff = df.groupby(['Year', 'Radius Location', 'Indicator Name']).mean('Value').reset_index()
+        else:
+            dff = df.groupby(['Year', 'Indicator Name']).mean('Value').reset_index()
+
+    else:
+        if radius_toggle == 'Inner/Outer CCG Breakdown':
+            dff = df.groupby(['Year', 'Radius Location', 'Indicator Name']).sum('Value').reset_index()
+        else:
+            dff = df.groupby(['Year', 'Indicator Name']).sum('Value').reset_index()
+
+    title = '{0} Across Time for NHS CCGs Regions'.format(indicator_name)
+
+    dff = dff[dff['Indicator Name'] == indicator_name]
+
+    # remove 0 at this point we wouldn't expect an average of 0 or sum of 0 ? (maybe though, double check)
+    dff = dff[dff['Value'] != 0]
+
+    fig122 = px.line(dff,
+                     x='Year',
+                     y='Value',
+                     color='Radius Location' if radius_toggle == 'Inner/Outer CCG Breakdown' else None,
+                     title=title)
+    fig122.update_yaxes(title=indicator_name)
+
+    return fig122
