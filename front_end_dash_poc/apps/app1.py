4c343a956784b80477460d164a2855739fc67fe5
Carla Daza <carladaza@Carlas-MacBook-Pro.local>
2021-01-21 22:37:57 -0500

    outer app1 and app2 WIP

--- front_end_dash_poc/apps/app1.py
+++ front_end_dash_poc/apps/app1.py
@@ -2,49 +2,70 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 import pandas as pd
-import dash
+import plotly.graph_objects as go
 import plotly.express as px
+import dash
 
 from app import app
 
-df = pd.read_csv('admission_pollution_melt.csv')
-available_indicators = df['Indicator Name'].unique()
+# mapbox styles that do not require a token: open-street-map, white-bg, carto-positron, carto-darkmatter, stamen-terrain, stamen-toner, stamen-watercolor
+# color scale informaiton: https://plotly.com/python/builtin-colorscales/
+
+#read file
+dfOuter = pd.read_csv('yearlyOuterStacked.csv').fillna(0)
+
+#date substring year
+dfOuter["Date"] = [int(str(x)[0:4]) for x in dfOuter["Date"]]
+
+#melt indicators to one column
+dfOuter = dfOuter.melt(id_vars = ["Date", "Location", "CCG", "Lat", "Long"], var_name = "Indicator", value_name = "Indicator Value")
+
+#create lists
+indicators = dfOuter['Indicator'].unique()
+locations = list(dfOuter['Location'].unique())
+locations.append('All Monitoring Sites')
 
 layout = html.Div([
-    html.H3('App 1 - Carla'),
-
-    # container for the two drop down options, (check how the id, correlates to the callback below)
+    html.H3('(DS4A Team 27 - Air Pollution and Health Ailments around Heathrow Airport - Pollution Levels by Monitoring Sites'),
     html.Div([
-            html.Div([
-                dcc.Dropdown(
-                    id='crossfilter-xaxis-column1',
-                    options=[{'label': i, 'value': i} for i in available_indicators],
-                    value='Asthma Admissions Over 19yr',
-                ),
-            ], style={'width': '49%', 'display': 'inline-block'}
-            ),
-            html.Div([
-                dcc.Dropdown(
-                    id='crossfilter-yaxis-column1',
-                    options=[{'label': i, 'value': i} for i in available_indicators],
-                    value='Asthma Admissions Over 19yr',
-                ),
-            ], style={'width': '49%', 'float': 'right', 'display': 'inline-block'})
-        ], style={
-            'borderBottom': 'thin lightgrey solid',
-            'backgroundColor': 'rgb(250, 250, 250)',
-            'padding': '10px 5px'
-        }),
 
-    # container for the graph (check how the id, correlates to the callback below)
+        html.Div([
+            dcc.Dropdown(
+                id='indicatorDropdown',
+                options=[{'label': i, 'value': i} for i in indicators],
+                value='Nitrogen dioxide',
+            ),
+        ],
+            style={'width': '48%', 'display': 'inline-block'}),
+
+        html.Div([
+            dcc.Dropdown(
+                id='monitoringSiteDropdown',
+                options=[{'label': i, 'value': i} for i in locations],
+                value='All Monitoring Sites',
+            ),
+        ], style={'width': '49%', 'float': 'right', 'display': 'inline-block'}),
+    ], style={
+        'borderBottom': 'thin lightgrey solid',
+        'backgroundColor': 'rgb(250, 250, 250)',
+        'padding': '10px 5px'
+    }),
+
     html.Div([
         dcc.Graph(
-            id='crossfilter-indicator-scatter1',
-            hoverData={'points': [{'customdata': 'NHS Richmond CCG'}]}
-        )
-    ],
-        # style={'width': '49%', 'display': 'inline-block', 'padding': '0 20'}   ---> commented out, you can edit the graph style via html this way
-    ),
+            id='monitoringSiteMap')
+    ]),
+
+    html.Div(dcc.Slider(
+        id='yearSlider',
+        min=dfOuter['Date'].min(),
+        max=dfOuter['Date'].max(),
+        value=dfOuter['Date'].max(),
+        marks={str(year): str(year) for year in dfOuter['Date'].unique()},
+        step=None
+    ), style={'width': '49%', 'padding': '0px 20px 20px 20px'}),
+
+    html.Div(id='app-1-display-value'),
 
     # Navigation Tree - Don't Delete
     html.Div(dcc.Link('Go to App 1', href='/apps/app1')),
@@ -55,21 +76,27 @@ layout = html.Div([
 
 ])
 
-# this call back enables interactivity via the drop down options, everytime the drop down is changed the df is rebuilt (see dff code below)
 @app.callback(
-    dash.dependencies.Output('crossfilter-indicator-scatter1', 'figure'),
-    [dash.dependencies.Input('crossfilter-xaxis-column1', 'value'),
-     dash.dependencies.Input('crossfilter-yaxis-column1', 'value'),
+    dash.dependencies.Output('monitoringSiteMap', 'figure'),
+    [dash.dependencies.Input('indicatorDropdown', 'value'),
+    dash.dependencies.Input('monitoringSiteDropdown', 'value'),
+     dash.dependencies.Input('yearSlider', 'value')
      ])
-def update_graph(xaxis_column_name, yaxis_column_name,):
-    dff = df
+def update_graph(indicator_val, monitoringsite_val,
+                 year_val):
 
-    fig = px.scatter(x=dff[dff['Indicator Name'] == xaxis_column_name]['Value'],
-            y=dff[dff['Indicator Name'] == yaxis_column_name]['Value'],
-            hover_name=dff[dff['Indicator Name'] == yaxis_column_name]['Area Name']
-            )
-    fig.update_traces(customdata=dff[dff['Indicator Name'] == yaxis_column_name]['Area Name'])
-    fig.update_layout(margin={'l': 40, 'b': 40, 't': 10, 'r': 0}, hovermode='closest')
+    if monitoringsite_val == 'All Monitoring Sites':
+        dff = dfOuter
+    else:
+        dff = dfOuter[dfOuter['Location'] == monitoringsite_val]
+
+    dff = dff[dff['Indicator'] == indicator_val]
+    dff = dff[dff['Date'] == year_val]
+
+    fig = px.scatter_mapbox(dff, lat="Lat", lon ="Long", color="Indicator Value", size="Indicator Value",
+                            color_continuous_scale=px.colors.diverging.Portland, size_max=30, zoom=7.5,
+                            hover_name='Location', hover_data=['Indicator', 'Indicator Value', 'Lat', 'Long'])
+
+    fig.update_layout(mapbox_style="carto-positron")
 
     return fig
-
--- front_end_dash_poc/apps/app2.py
+++ front_end_dash_poc/apps/app2.py
@@ -4,32 +4,73 @@ from dash.dependencies import Input, Output
 import pandas as pd
 import dash
 import plotly.express as px
+import math
 
 from app import app
 
-df = pd.read_csv('admission_pollution_melt.csv')
-available_indicators = df['Indicator Name'].unique()
+#read file
+dfOuter = pd.read_csv('yearlyOuterStacked.csv').fillna(0)
+
+#date substring year
+dfOuter["Date"] = [int(str(x)[0:4]) for x in dfOuter["Date"]]
+
+#heathrow distance
+heathrow_lat, heathrow_lon = 51.47002, -0.454295
+
+#calculate distance from monitoring sites
+def getDistance(x,y):
+    xSquared = (heathrow_lat - x) * (heathrow_lat - x)
+    ySquared = (heathrow_lon - y) * (heathrow_lon - y)
+    radicand = xSquared + ySquared
+    return math.sqrt(radicand)
+dfOuter["Distance"] = [getDistance(x,y) for x, y in zip(dfOuter["Lat"], dfOuter["Long"])]
+
+#melt indicators to one column
+dfOuter = dfOuter.melt(id_vars = ["Date", "Location", "CCG", "Lat", "Long", "Distance"], var_name = "Indicator", value_name = "Indicator Value")
+
+#create lists
+indicators = dfOuter['Indicator'].unique()
+locations = list(dfOuter['Location'].unique())
+locations.append('All Monitoring Sites')
+
+#wgs84_geod = Geod(ellps='WGS84')
+
+#def get_distance(lat1, lon1, lat2, lon2):
+        # distance is returned in metres (cross-checked on site)
+#        az12,az21,dist = wgs84_geod.inv(lon1,lat1,lon2,lat2)
+#        return(dist)
+
+#def calc_heathrow_dist(row):
+#    return get_distance(heathrow_lat, heathrow_lon, row['Lat'], row['Long'])
+    
+    
+#def calc_heathrow_dist_from_latlong(lat_long):
+#    try:
+#        return get_distance(heathrow_lat, heathrow_lon, lat_long[0], lat_long[1])
+#    except TypeError:
+#        print(lat_long)
+#    return 'nothing'
 
 layout = html.Div([
-    html.H3('App 2 - Carla/Kayli'),
+    html.H3('Monitoring Site Values vs. Distance'),
 
     # container for the two drop down options, (check how the id, correlates to the callback below)
     html.Div([
             html.Div([
                 dcc.Dropdown(
-                    id='crossfilter-xaxis-column2',
-                    options=[{'label': i, 'value': i} for i in available_indicators],
-                    value='Asthma Admissions Over 19yr',
+                    id='xaxis',
+                    options=[{'label': i, 'value': i} for i in indicators],
+                    value='Nitrogen dioxide',
                 ),
             ], style={'width': '49%', 'display': 'inline-block'}
             ),
-            html.Div([
-                dcc.Dropdown(
-                    id='crossfilter-yaxis-column2',
-                    options=[{'label': i, 'value': i} for i in available_indicators],
-                    value='Asthma Admissions Over 19yr',
-                ),
-            ], style={'width': '49%', 'float': 'right', 'display': 'inline-block'})
+            #html.Div([
+            #    dcc.Dropdown(
+            #        id='yaxis',
+            #        options=[{'label': i, 'value': i} for i in available_indicators],
+            #        value='Asthma Admissions Over 19yr',
+            #    ),
+            #], style={'width': '49%', 'float': 'right', 'display': 'inline-block'})
         ], style={
             'borderBottom': 'thin lightgrey solid',
             'backgroundColor': 'rgb(250, 250, 250)',
@@ -39,8 +80,8 @@ layout = html.Div([
     # container for the graph (check how the id, correlates to the callback below)
     html.Div([
         dcc.Graph(
-            id='crossfilter-indicator-scatter2',
-            hoverData={'points': [{'customdata': 'NHS Richmond CCG'}]}
+            id='monitoringSiteDistance',
+            hoverData={'points': [{'customdata': 'Oxford'}]}
         )
     ],
         # style={'width': '49%', 'display': 'inline-block', 'padding': '0 20'}   ---> commented out, you can edit the graph style via html this way
@@ -57,18 +98,18 @@ layout = html.Div([
 
 # this call back enables interactivity via the drop down options, everytime the drop down is changed the df is rebuilt (see dff code below)
 @app.callback(
-    dash.dependencies.Output('crossfilter-indicator-scatter2', 'figure'),
-    [dash.dependencies.Input('crossfilter-xaxis-column2', 'value'),
-     dash.dependencies.Input('crossfilter-yaxis-column2', 'value'),
+    dash.dependencies.Output('monitoringSiteDistanxe', 'figure'),
+    [dash.dependencies.Input('xaxis', 'value'),
+     #dash.dependencies.Input('crossfilter-yaxis-column2', 'value'),
      ])
-def update_graph(xaxis_column_name, yaxis_column_name,):
-    dff = df
+def update_graph(xaxis):#, yaxis,):
+    dff = dfOuter
 
-    fig = px.scatter(x=dff[dff['Indicator Name'] == xaxis_column_name]['Value'],
-            y=dff[dff['Indicator Name'] == yaxis_column_name]['Value'],
-            hover_name=dff[dff['Indicator Name'] == yaxis_column_name]['Area Name']
+    fig = px.scatter(x=dff[dff['Indicator Name'] == xaxis]['Value'],
+            y=dff[dff['Indicator Value']]['Value'],
+            hover_name=dff[dff['Indicator Name'] == xaxis]['Location']
             )
-    fig.update_traces(customdata=dff[dff['Indicator Name'] == yaxis_column_name]['Area Name'])
+    #fig.update_traces(customdata=dff[dff['Indicator Name'] == yaxis_column_name]['Area Name'])
     fig.update_layout(margin={'l': 40, 'b': 40, 't': 10, 'r': 0}, hovermode='closest')
 
     return fig
