3c195441a5e50eb351843f1a1a7eb7b6d0b32059
Kayli Wren <70422187+kayliwren@users.noreply.github.com>
2021-01-24 18:40:30 -0500

    updated app3 - health and distance

--- front_end_dash_poc/apps/app3.py
+++ front_end_dash_poc/apps/app3.py
@@ -1,3 +1,5 @@
+# Health and Distance
+
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
@@ -7,44 +9,27 @@ import plotly.express as px
 
 from app import app
 
-df = pd.read_csv('admission_pollution_melt.csv')
+df = pd.read_csv('health_data_final.csv')
 available_indicators = df['Indicator Name'].unique()
 
 layout = html.Div([
-    html.H3('App 3 - Kayli'),
+    html.H3('App 3 - Kayli - Health Indicators vs Distance from Airport'),
 
-    # container for the two drop down options, (check how the id, correlates to the callback below)
     html.Div([
-            html.Div([
-                dcc.Dropdown(
-                    id='crossfilter-xaxis-column3',
-                    options=[{'label': i, 'value': i} for i in available_indicators],
-                    value='Asthma Admissions Over 19yr',
-                ),
-            ], style={'width': '49%', 'display': 'inline-block'}
-            ),
-            html.Div([
-                dcc.Dropdown(
-                    id='crossfilter-yaxis-column3',
-                    options=[{'label': i, 'value': i} for i in available_indicators],
-                    value='Asthma Admissions Over 19yr',
-                ),
-            ], style={'width': '49%', 'float': 'right', 'display': 'inline-block'})
-        ], style={
-            'borderBottom': 'thin lightgrey solid',
-            'backgroundColor': 'rgb(250, 250, 250)',
-            'padding': '10px 5px'
-        }),
+        dcc.Dropdown(
+            id='indicator-column',
+            options=[{'label': i, 'value': i} for i in available_indicators],
+            value='Heart Failure Admissions',
+        ),
+    ],
+        style={'width': '48%', 'display': 'inline-block'}),
 
-    # container for the graph (check how the id, correlates to the callback below)
+
     html.Div([
         dcc.Graph(
-            id='crossfilter-indicator-scatter3',
-            hoverData={'points': [{'customdata': 'NHS Richmond CCG'}]}
+            id='crossfilter-indicator-scattermap-k'
         )
-    ],
-        # style={'width': '49%', 'display': 'inline-block', 'padding': '0 20'}   ---> commented out, you can edit the graph style via html this way
-    ),
+    ]),
 
     # Navigation Tree - Don't Delete
     html.Div(dcc.Link('Go to App 1', href='/apps/app1')),
@@ -57,19 +42,14 @@ layout = html.Div([
 
 # this call back enables interactivity via the drop down options, everytime the drop down is changed the df is rebuilt (see dff code below)
 @app.callback(
-    dash.dependencies.Output('crossfilter-indicator-scatter3', 'figure'),
-    [dash.dependencies.Input('crossfilter-xaxis-column3', 'value'),
-     dash.dependencies.Input('crossfilter-yaxis-column3', 'value'),
+    dash.dependencies.Output('crossfilter-indicator-scattermap-k', 'figure'),
+    [dash.dependencies.Input('indicator-column', 'value')
      ])
-def update_graph(xaxis_column_name, yaxis_column_name,):
-    dff = df
+def update_graph(indicator_name):
+    temp_df = df[df["Indicator Name"] == indicator_name]
 
-    fig = px.scatter(x=dff[dff['Indicator Name'] == xaxis_column_name]['Value'],
-            y=dff[dff['Indicator Name'] == yaxis_column_name]['Value'],
-            hover_name=dff[dff['Indicator Name'] == yaxis_column_name]['Area Name']
-            )
-    fig.update_traces(customdata=dff[dff['Indicator Name'] == yaxis_column_name]['Area Name'])
-    fig.update_layout(margin={'l': 40, 'b': 40, 't': 10, 'r': 0}, hovermode='closest')
+    fig = px.scatter(temp_df, x="heathrow_distance", y="Value", trendline="ols",
+        labels=dict(heathrow_distance="Distance from Heathrow Airport",
+            Value="Rate of Health Indicator per 100k People"))
 
     return fig
-
